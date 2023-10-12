c0f905d0bd1f5519b431a3f133ec8aed655b13df
the0therguy <nblues399@gmail.com>
2023-10-12 11:49:31 +0600

    Return api response updated

--- vat_automation_app/views.py
+++ vat_automation_app/views.py
@@ -480,24 +480,25 @@ class ReturnView(APIView):
         personal_details = PersonalDetails.objects.get(user=request.user)
         if not personal_details:
             return Response("No personal details found", status=status.HTTP_400_BAD_REQUEST)
-        return_details = {}
+        basic_info = {}
         assesment_year = str(datetime.now().year) + '-' + str(datetime.now().year + 1)[2:]
-        return_details['Name of the Assessee'] = personal_details.assess_name
-        return_details['TIN'] = personal_details.tin
-        return_details['nid'] = personal_details.nid
-        return_details['passport'] = personal_details.passport_number
-        return_details['Assessment Year'] = assesment_year
-        return_details['Circle'] = personal_details.circle
-        return_details['Tax Zone'] = personal_details.tax_zone
-        return_details['Resident Status'] = personal_details.resident_status
-        return_details['tick_box'] = personal_details.are_you
-        return_details['address'] = personal_details.address
-        return_details['date_of_birth'] = personal_details.date_of_birth
-        return_details['telephone'] = request.user.phone_number
-        return_details['email'] = request.user.email
-        return_details['year_ended_on'] = personal_details.income_year_ended_on
+        basic_info['Name of the Assessee'] = personal_details.assess_name
+        basic_info['TIN'] = personal_details.tin
+        basic_info['nid'] = personal_details.nid
+        basic_info['passport'] = personal_details.passport_number
+        basic_info['Assessment Year'] = assesment_year
+        basic_info['Circle'] = personal_details.circle
+        basic_info['Tax Zone'] = personal_details.tax_zone
+        basic_info['Resident Status'] = personal_details.resident_status
+        basic_info['tick_box'] = personal_details.are_you
+        basic_info['address'] = personal_details.address
+        basic_info['date_of_birth'] = personal_details.date_of_birth
+        basic_info['telephone'] = request.user.phone_number
+        basic_info['email'] = request.user.email
+        basic_info['year_ended_on'] = personal_details.income_year_ended_on
         salary_government = self.get_transaction(user=request.user, category_name='Salary Government',
                                                  year=assesment_year)
+        particulars_of_income = {}
         salary = Decimal(0)
         if salary_government:
             salary += salary_government.taxable_income
@@ -506,58 +507,74 @@ class ReturnView(APIView):
         if salary_private:
             salary += salary_private.taxable_income
 
-        return_details['Income from Salaries (annex Schedule 1)'] = salary
+        particulars_of_income['Income from Salaries (annex Schedule 1)'] = salary
 
         rent = self.get_transaction(user=request.user, category_name='House Income',
                                     year=assesment_year)
         if rent:
-            return_details['Income from Rent (annex Schedule 2)'] = rent.taxable_income
+            particulars_of_income['Income from Rent (annex Schedule 2)'] = rent.taxable_income
         else:
-            return_details['Income from Rent (annex Schedule 2)'] = float(0)
+            particulars_of_income['Income from Rent (annex Schedule 2)'] = float(0)
         agriculture = self.get_transaction(user=request.user, category_name='Agriculture', year=assesment_year)
         if agriculture:
-            return_details['Agricultural income (annex Schedule 3)'] = agriculture.taxable_income
+            particulars_of_income['Agricultural income (annex Schedule 3)'] = agriculture.taxable_income
         else:
-            return_details['Agricultural income (annex Schedule 3)'] = float(0)
+            particulars_of_income['Agricultural income (annex Schedule 3)'] = float(0)
         business = self.get_transaction(user=request.user, category_name='Business', year=assesment_year)
         if business:
-            return_details['Income from business (annex Schedule 4)'] = business.taxable_income
+            particulars_of_income['Income from business (annex Schedule 4)'] = business.taxable_income
         else:
-            return_details['Income from business (annex Schedule 4)'] = float(0)
+            particulars_of_income['Income from business (annex Schedule 4)'] = float(0)
 
-        return_details['Capital gains'] = 0
-        return_details['Income from Financial Assets (Bank interest/profit, Dividend, Securities etc.)'] = 0
-        return_details['Income from other sources (Royalty, License fee, Honorarium, Fees, Govt. Incentive etc.)'] = 0
-        return_details['Share of income from firm or AOP'] = 0
-        return_details['Income of minor or spouse under section (if not assessee)'] = 0
-        return_details['Foreign income'] = 0
+        particulars_of_income['Capital gains'] = 0
+        particulars_of_income['Income from Financial Assets (Bank interest/profit, Dividend, Securities etc.)'] = 0
+        particulars_of_income[
+            'Income from other sources (Royalty, License fee, Honorarium, Fees, Govt. Incentive etc.)'] = 0
+        particulars_of_income['Share of income from firm or AOP'] = 0
+        particulars_of_income['Income of Minor or Spouse (if not Taxpayer)'] = 0
+        particulars_of_income['Taxable Income from Abroad'] = 0
+
+        tax_consumption = {}
+        particulars_of_tax_payment = {}
         report = self.get_report(user=request.user)
         if not report:
             return Response('Please fill report first', status=status.HTTP_400_BAD_REQUEST)
-        return_details['Gross tax on taxable Income '] = report.taxable_income
+        tax_consumption['Gross tax on taxable Income '] = report.taxable_income
         if personal_details.resident_status == 'Non-Resident':
-            return_details['Tax rebate (annex Schedule 5)'] = float(0)
-            return_details['Net tax after tax rebate (12-13)'] = 0
-            return_details['Minimum tax'] = 0
-            return_details['Tax Payable (Higher of 14 and 15)'] = 0
-            return_details['Net wealth surcharge (if applicable)'] = 0
-            return_details['Environmental surcharge (if applicable)'] = 0
-            return_details['Delay Interest, Penalty or any other amount under the Income Tax Act (if any)'] = 0
-            return Response(return_details, status=status.HTTP_200_OK)
+            tax_consumption['Tax rebate (annex Schedule 5)'] = float(0)
+            tax_consumption['Net tax after tax rebate (12-13)'] = 0
+            tax_consumption['Minimum tax'] = 0
+            tax_consumption['Tax Payable (Higher of 14 and 15)'] = 0
+            tax_consumption['Net wealth surcharge (if applicable)'] = 0
+            tax_consumption['Environmental surcharge (if applicable)'] = 0
+            tax_consumption['Delay Interest, Penalty or any other amount under the Income Tax Act (if any)'] = 0
+            particulars_of_tax_payment['Tax Deducted or Collected at Source (attach proof)'] = 0
+            particulars_of_tax_payment['Advance Tax paid (attach proof)'] = 0
+            particulars_of_tax_payment['Adjustment of Tax Refund {mention assessment year(s) of refund} '] = 0
+            particulars_of_tax_payment['Tax Paid with this Return'] = 0
+            return Response({'Basic Info': basic_info, 'Particulars of Income': particulars_of_income,
+                             'Tax Consumption': tax_consumption,
+                             'Particulars of Tax Payment': particulars_of_tax_payment}, status=status.HTTP_200_OK)
 
-        return_details['Tax rebate (annex Schedule 5)'] = report.rebate
-        return_details['Net tax after tax rebate (12-13)'] = abs(report.taxable_income - report.rebate)
+        tax_consumption['Tax rebate (annex Schedule 5)'] = report.rebate
+        tax_consumption['Net tax after tax rebate (12-13)'] = abs(report.taxable_income - report.rebate)
         if report.net_tax == float(0):
             minimum_tax = float(0)
         elif float(1) <= report.net_tax <= float(5000):
             minimum_tax = float(5000)
         else:
             minimum_tax = report.net_tax
-        return_details['Minimum tax '] = minimum_tax
-        return_details['Tax Payable (Higher of 14 and 15)'] = max(minimum_tax,
-                                                                  abs(report.taxable_income - report.rebate))
-        return_details['Net wealth surcharge (if applicable)'] = 0
-        return_details['Environmental surcharge (if applicable)'] = 0
-        return_details['Delay Interest, Penalty or any other amount under the Income Tax Act (if any)'] = 0
+        tax_consumption['Minimum tax '] = minimum_tax
+        tax_consumption['Tax Payable (Higher of 14 and 15)'] = max(minimum_tax,
+                                                                   abs(report.taxable_income - report.rebate))
+        tax_consumption['Net wealth surcharge (if applicable)'] = 0
+        tax_consumption['Environmental surcharge (if applicable)'] = 0
+        tax_consumption['Delay Interest, Penalty or any other amount under the Income Tax Act (if any)'] = 0
 
-        return Response(return_details, status=status.HTTP_200_OK)
+        particulars_of_tax_payment['Tax Deducted or Collected at Source (attach proof)'] = 0
+        particulars_of_tax_payment['Advance Tax paid (attach proof)'] = 0
+        particulars_of_tax_payment['Adjustment of Tax Refund {mention assessment year(s) of refund} '] = 0
+        particulars_of_tax_payment['Tax Paid with this Return'] = 0
+        return Response({'Basic Info': basic_info, 'Particulars of Income': particulars_of_income,
+                         'Tax Consumption': tax_consumption,
+                         'Particulars of Tax Payment': particulars_of_tax_payment}, status=status.HTTP_200_OK)
